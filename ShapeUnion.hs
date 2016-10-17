87cdb34ae70ab8554565c036bf6e90c8fbc0f8ee
Hungai Amuhinda <hungaiinc@gmail.com>
2016-10-17 19:38:19 +0300

    More on Custom Types

--- ShapeUnion.hs
+++ ShapeUnion.hs
@@ -1,5 +1,10 @@
 
 type Vector = (Double, Double)
 
-data Shape = Circle Vector Double 
-            | Poly [Vector]
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+    deriving (Show)
+
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r^2 
+area (Rectangle x y x2 y2) = (abs(x2-x))* (abs(y2 - y))

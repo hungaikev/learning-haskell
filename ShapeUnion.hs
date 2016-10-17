7f5f80bf9936ca11c50b86f8ae6b8c450b21012a
Hungai Amuhinda <hungaiinc@gmail.com>
2016-10-17 19:42:42 +0300

    Area of shapes

--- ShapeUnion.hs
+++ ShapeUnion.hs
@@ -8,3 +8,7 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 area :: Shape -> Float
 area (Circle _ _ r) = pi * r^2 
 area (Rectangle x y x2 y2) = (abs(x2-x))* (abs(y2 - y))
+
+
+areaOfCircle = area(Circle 50 60 20)
+areaOfRectangle = area $ Rectangle 10 10 100 100 

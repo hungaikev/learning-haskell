44f1b3280ddd92cceb16f32306ac25afa32f62ef
Hungai Amuhinda <hungaiinc@gmail.com>
2016-10-24 18:44:06 +0300

    Pattern matching and Using Guards

--- cis194/week1/intro.hs
+++ cis194/week1/intro.hs
@@ -12,3 +12,22 @@ reallyBig = 2^(2^(2^(2^2)))
 
 numDigits :: Int 
 numDigits = length(show reallyBig)
+ex11 = True && False 
+ex12 = not(False || True)
+ex13 = ('a' == 'a')
+ex14 = (16 /= 3)
+ex15 = (5>3) && ('p' <= 'q')
+ex16 = "Haskell" > "C++"
+
+
+--Pattern matching 
+--Recursive function 
+sumtorial :: Int -> Int 
+sumtorial 0 = 0 
+sumtorial n = n + sumtorial (n-1)
+
+--Using Guards
+hailstone :: Int -> Int 
+hailstone n 
+    | n `mod` 2 == 0 = n `div` 2 
+    | otherwise  = 3*n+1 

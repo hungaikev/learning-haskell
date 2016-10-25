03537023a4f6121039e122013a1235412e3c48e9
Hungai Amuhinda <hungaiinc@gmail.com>
2016-10-25 20:50:50 +0300

    Ref

--- cis194/week1/homework.hs
+++ cis194/week1/homework.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wall #-}
+
+{-# LANGUAGE ViewPatterns #-}
+
 -- Converts positive Integers to a list of their digits.
 -- toDigits 1234 == [1,2,3,4]
 -- toDigits 0 == []
@@ -8,7 +12,6 @@ toDigits n
 
 -- Converts positive Integers to a reversed list of their digits.
 -- toDigits 1234 == [4,3,2,1]
-
 toDigitsRev n 
     | n > 0 = n `mod` 10 : toDigitsRev (n `div` 10)
     | otherwise = []
@@ -18,7 +21,28 @@ toDigitsRev n
 -- Doubles every other number beginning from the rights
 -- doubleEveryOther [8,7,6,5] == [16,7,12,5]
 -- doubleEveryOther [1,2,3] == [1,4,3]
+
 doubleEveryOther :: [Int] -> [Int]
-doubleEveryOther [] = []
-doubleEveryOther (x:[]) = [x]
-doubleEveryOther (x:(y:zs)) = x : y*2 : doubleEveryOther zs 
+doubleEveryOther (reverse -> (x:y:zs)) =
+  doubleEveryOther (reverse zs) ++ [2*y, x]
+doubleEveryOther _ = []
+
+doubleEveryOther2 :: [Int] -> [Int]
+doubleEveryOther2 [] = []
+doubleEveryOther2 (x:[]) = [x]
+doubleEveryOther2 (x:(y:zs)) = x : y*2 : doubleEveryOther zs 
+
+-- Sum over all digits of a list of Integers
+-- sumDigits [16,7,12,5] = 1 + 6 + 7 + 1 + 2 + 5 = 22
+sumDigits :: [Integer] -> Integer
+sumDigits lst = sum $ map (sum . toDigits) lst
+
+
+-- Return True for a valid credit card number
+-- validate 4012888888881881 = True
+-- validate 4012888888881882 = False
+-- validate -4012888888881881 = False
+validate :: Integer -> Bool
+validate n
+  | n > 0     = mod (sumDigits . doubleEveryOther . toDigits $ n) 10 == 0
+  | otherwise = False

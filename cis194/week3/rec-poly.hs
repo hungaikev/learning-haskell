01dd7fd4ced2bf103a8f7a2bc52c04044623ec8b
Hungai Amuhinda <hungaiinc@gmail.com>
2016-10-27 11:30:39 +0300

    Mapping and filtering

--- cis194/week3/rec-poly.hs
+++ cis194/week3/rec-poly.hs
@@ -15,3 +15,23 @@ absAll (Cons x xs) = Cons(abs x) (absAll xs)
 squareAll :: IntList -> IntList
 squareAll Empty = Empty
 squareAll (Cons x xs) = Cons (x * x)(squareAll xs)
+
+
+exampleList = Cons (-1) (Cons 2 (Cons (-6) Empty))
+
+addOne x = x + 1
+square x = x * x  
+
+
+mapIntList :: (Int -> Int) -> IntList -> IntList
+mapIntList _ Empty = Empty
+mapIntList f (Cons x xs) = Cons (f x) (mapIntList f xs)
+
+
+keepOnlyEven :: IntList -> IntList
+keepOnlyEven Empty  = Empty
+keepOnlyEven (Cons x xs)
+    | even x = Cons x (keepOnlyEven xs)
+    | otherwise = keepOnlyEven xs
+
+
